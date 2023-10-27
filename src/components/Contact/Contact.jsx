4dfc724e2c702bfc2432d2e9a9d7812f3251ce21
Ganiramadhan <ganiramadhan22@gmail.com>
2023-10-27 09:10:16 +0700

    Upload My Portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">ganiramadhan22@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="#">linkedin.com/ganiramadhan</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Ganiramadhan">github.com/ganiramadhan</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
